085070bc462eb3a8e0cff268e4546698fb171dfd
Egor Romanov <unsaidxpl@gmail.com>
2022-07-26 23:53:06 +0300

    Try to implement elixir with functions

--- elixir/httpb.exs
+++ elixir/httpb.exs
@@ -1,13 +1,21 @@
 Mix.install([{:simplehttp, "~> 0.5.1"}])
 
-[uri] = System.argv
+[uri, number] = System.argv
+{limit,_} = Integer.parse(number)
 
-start_at = Time.utc_now
-case SimpleHttp.get(uri) do
-  {:ok, _ } ->
-    elapsed = Time.diff(Time.utc_now, start_at, :millisecond)
-    IO.puts("Fetched in #{elapsed} ms")
+fetch_uri = fn uri ->  SimpleHttp.get(uri) end
+do_request = fn _ ->
+  start_at = Time.utc_now
+  case fetch_uri.(uri) do
+    {:ok, _ } ->
+      elapsed = Time.diff(Time.utc_now, start_at, :millisecond)
+      IO.puts("Fetched in #{elapsed} ms")
+      {:ok, elapsed}
 
-  {:error, reason } ->
-    IO.puts("Failed: #{reason}")
+    {:error, reason } ->
+      IO.puts("Failed: #{reason}")
+      {:error, nil}
+  end
 end
+
+Enum.map(1..limit, do_request)
